8de7613fb6974f86fc34e35ba30a98f9b424bc24
ZheHacK <54292567+ZheHacK@users.noreply.github.com>
2023-07-19 22:58:46 +0700

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,3 +1,29 @@
+const fetch = require('node-fetch');
+
+const getVideoToken = async () => {
+  const tv = prompt('Masukkan ID TV: ');
+  const tokke = `https://www.vidio.com/live/${tv}/tokens`;
+
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'charset': 'utf-8',
+  };
+
+  const response = await fetch(tokke, {
+    method: 'POST',
+    headers: headers,
+  });
+
+  const result = await response.json();
+  const token = result.token;
+
+  console.log(token);
+};
+
+getVideoToken();
+
+
+
 // hello there!
 // 
 // I'm a serverless function that you can deploy as part of your site.
@@ -6,10 +32,10 @@
 // of your site. Netlify Functions will handle the rest for you.
 
 
-exports.handler = async event => {
-    const subject = event.queryStringParameters.name || 'World ZheHacK'
-    return {
-        statusCode: 200,
-        body: `Hello ${subject}!`,
-    }
-}
+//exports.handler = async event => {
+//    const subject = event.queryStringParameters.name || 'World ZheHacK'
+//    return {
+//        statusCode: 200,
+//        body: `Hello ${subject}!`,
+//    }
+//}
